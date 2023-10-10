4d68409971343dbd776a67e1b9bc8ee41dbe5a60
Gerard Llorach <gerard.llorach@gmail.com>
2023-10-10 13:14:27 +0200

    found the formula to define steepness with period

--- main.js
+++ main.js
@@ -7,31 +7,37 @@ const oceanParameters = [
         steep: 0.2,
         angle: 90
     },    
-    {
-        hm0: 1,
-        steep: 0.2,
-        angle: 90
-    },
-    {
-        hm0: 0.2,
-        steep: 0.35,
-        angle: -60
-    },
-    {
-        hm0: 0.18,
-        steep: 0.35,
-        angle: -60
-    },
-    {
-        hm0: 0.21,
-        steep: 0.05,
-        angle: -60
-    },
+    // {
+    //     hm0: 1,
+    //     steep: 0.2,
+    //     angle: 90
+    // },
+    // {
+    //     hm0: 0.2,
+    //     steep: 0.35,
+    //     angle: -60
+    // },
+    // {
+    //     hm0: 0.18,
+    //     steep: 0.35,
+    //     angle: -60
+    // },
+    // {
+    //     hm0: 0.21,
+    //     steep: 0.05,
+    //     angle: -60
+    // },
 ]
 
 
-const sampleRate = 20;
-const seconds = 60*2;
+const sampleRate = 30;
+const seconds = 60*3;
+
+let T = 10;
+let A = oceanParameters[0].hm0*0.5;
+let s = 4 * Math.PI * Math.PI * A / (T * T * 9.8);
+console.log(s);
+oceanParameters[0].steep = s;
 
 
 let oceanSpec = new OceanSpectrum(sampleRate, seconds);
